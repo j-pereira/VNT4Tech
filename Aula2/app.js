628be3b359901c4ae0b26e88fb8dd0b5bdcdf60e
Jessica Pereira <jessica.pereira1764@gmail.com>
2018-07-20 13:12:48 -0300

    gitignore

--- Aula2/app.js
+++ Aula2/app.js
@@ -3,6 +3,11 @@
 const app = require('./config/server');
 const port = 3000;
 
+server.use('/vjobs', express.static(__dirname + '/app/static'));
+server.get('/', async (req, res) => {
+    return res.redirect('http://localhost:3000/vjobs/index.html');
+});
+
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
--- Aula2/config/server.js
+++ Aula2/config/server.js
@@ -4,9 +4,6 @@ const express = require('express');
 const server = express();
 const bodyParser = require('body-parser');     
 const consign = require('consign');
-
-
-server.use('/vjobs', express.static(__dirname + '/app/static'));
               
 server.use(bodyParser.urlencoded({ extended : false }));  
 server.use(bodyParser.json());                                     
@@ -14,9 +11,5 @@ server.use(bodyParser.json());
 consign()
     .include('./app/routes')
     .into(server);
-
-server.get('/', async (req, res) => {
-    return res.redirect('http://localhost:3000/vjobs/index.html');
-});
 
 module.exports = server;
